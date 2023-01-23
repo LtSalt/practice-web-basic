dbc5ab6912cb09253fe2a7b3ce3c18f6af33b5fb
David Will <pfefferundsalz@posteo.de>
2023-01-23 13:22:26 +0100

    Removed unnecessary callback

--- notes-app/index.js
+++ notes-app/index.js
@@ -5,19 +5,15 @@ const note = document.createElement('div');
 note.classList.add('note');
 note.innerHTML = '<div class="noteHeader"><div class="noteTrash"></div></div><textarea class="noteCanvas">';
 
-addNote.addEventListener('click', function() {
-    console.log('clicked me');
-    createNote();
-});
-
+addNote.addEventListener('click', createNote);
 main.addEventListener('click', removeNote); 
-
-function removeNote(event) {
-    const trash = event.target.parentNode.parentNode;
-    if (trash.classList.contains('note')) trash.remove();
-};
 
 function createNote() {
     const newNote = note.cloneNode(deep = true);
     main.appendChild(newNote);
+};
+
+function removeNote(event) {
+    const trash = event.target.parentNode.parentNode;
+    if (trash.classList.contains('note')) trash.remove();
 };
